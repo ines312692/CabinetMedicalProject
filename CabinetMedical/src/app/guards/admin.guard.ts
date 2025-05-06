45e979e949501d9c1502bfc4b92d8534dee67f9c
ines Tmimi <ines.tmimi@ensit.u-tunis.tn>
2025-05-06 20:43:07 +0100

    add

---
+++ CabinetMedical/src/app/guards/admin.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private readonly authService: AuthService, private readonly router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.getUserRole() === 'admin') {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+    console.log('AdminGuard: User is not an admin, redirecting to login.');
+  }
+
+}
